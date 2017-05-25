9734ae2b2151a0e02ae313395c6667e323e5f3cc
Davide <daviderusso17@libero.it>
2017-05-25 17:40:57 +0200

    Merge with Master

--- src/main/java/it/finsoft/resources/WSCollector.java
+++ src/main/java/it/finsoft/resources/WSCollector.java
@@ -2,15 +2,12 @@ package it.finsoft.resources;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-import it.finsoft.entity.Entita;
 import it.finsoft.entity.Evento;
 import it.finsoft.manager.EntitaManager;
 import it.finsoft.manager.EventoManager;
