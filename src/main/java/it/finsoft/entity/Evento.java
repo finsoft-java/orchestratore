7ed225658df19ea0808573742fec87708603d0d0
finsoft <finsoft@192.168.1.227>
2017-05-26 10:51:15 +0200

    new index

--- src/main/java/it/finsoft/entity/Evento.java
+++ src/main/java/it/finsoft/entity/Evento.java
@@ -14,14 +14,12 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-import org.hibernate.service.spi.Startable;
 
 @Entity
 @Table(name = "EVENTI")
