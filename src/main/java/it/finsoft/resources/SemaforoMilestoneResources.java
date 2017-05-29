1d99da6e03bae06a53efe39d645e820a1985fb5d
Finsoft-PC <Finsoft-PC@192.168.1.221>
2017-05-29 15:18:28 +0200

    Rimossi ultimi import non utilizzati

--- src/main/java/it/finsoft/resources/SemaforoMilestoneResources.java
+++ src/main/java/it/finsoft/resources/SemaforoMilestoneResources.java
@@ -12,11 +12,8 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-import it.finsoft.entity.Semaforo;
 import it.finsoft.entity.SemaforoMilestone;
 import it.finsoft.manager.SemaforoMilestoneManager;
 
