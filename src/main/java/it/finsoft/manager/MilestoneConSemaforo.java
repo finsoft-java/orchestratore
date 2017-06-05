8e6cd52ca4649b2c48917f6c648b209cdf992b35
finsoft <finsoft@192.168.1.227>
2017-06-05 15:44:00 +0200

    nuova classe MilestoneConSemaforo

--- src/main/java/it/finsoft/manager/MilestoneConSemaforo.java
+++ src/main/java/it/finsoft/manager/MilestoneConSemaforo.java
@@ -1,30 +1,29 @@
 package it.finsoft.manager;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import it.finsoft.entity.Azione;
+import it.finsoft.entity.Entita;
+import it.finsoft.entity.Evento;
 import it.finsoft.entity.Milestone;
+import it.finsoft.entity.TipoEvento;
 
 public class MilestoneConSemaforo extends Milestone {
 
 	private static final long serialVersionUID = 5115044789737668255L;
-	private Milestone milestone;
+	// private Milestone milestone;
 	private boolean semaforo;
-	private List<String> tags;
-
+	private List<String> tags = new ArrayList<>();
+	private List<Evento> eventi = new ArrayList<>();
 	public MilestoneConSemaforo() {
 	}
 
-	public MilestoneConSemaforo(Milestone milestone, boolean semaforo, List<String> tags) {
-		this.milestone = milestone;
+	public MilestoneConSemaforo(Long idMilestone, TipoEvento tipoEvento, Entita entita, String descrizione,
+			Azione azione, boolean semaforo, List<String> tags, List<Evento> eventi) {
 		this.semaforo = semaforo;
 		this.tags = tags;
-	}
-
-	public Milestone getMilestone() {
-		return milestone;
-	}
-
-	public void setMilestone(Milestone milestone) {
-		this.milestone = milestone;
+		this.eventi=eventi;
 	}
 
 	public boolean isSemaforo() {
@@ -41,6 +40,14 @@ public class MilestoneConSemaforo extends Milestone {
 
 	public void setTags(List<String> tags) {
 		this.tags = tags;
+	}
+
+	public List<Evento> getEventi() {
+		return eventi;
+	}
+
+	public void setEventi(List<Evento> eventi) {
+		this.eventi = eventi;
 	}
 
 }
