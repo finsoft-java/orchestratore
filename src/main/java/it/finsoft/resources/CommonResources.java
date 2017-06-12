cdc4864d764ecaa18ef0ff05510b8df57e7912c1
Luca <luca.vercelli.to@gmail.com>
2017-06-12 17:21:56 +0200

    typo

--- src/main/java/it/finsoft/resources/CommonResources.java
+++ src/main/java/it/finsoft/resources/CommonResources.java
@@ -29,7 +29,7 @@ import it.finsoft.manager.CommonManager;
 
 /**
  * This REST WS handles not just one entity, but all possible entities. Entites
- * can lie in different packages, however their nemaes must be different (I
+ * can lie in different packages, however their names must be different (I
  * argue this is assumed by JPA too). This service assumes all entities have a
  * primary key of type Long, called Id.
  * 
