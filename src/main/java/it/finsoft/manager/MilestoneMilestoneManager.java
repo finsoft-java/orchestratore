5a75a7d00e76ffaa181d0ae5e720d974f39d8c80
Davide <daviderusso17@libero.it>
2017-06-12 09:29:39 +0200

    fix funzione

--- src/main/java/it/finsoft/manager/MilestoneMilestoneManager.java
+++ src/main/java/it/finsoft/manager/MilestoneMilestoneManager.java
@@ -44,21 +44,18 @@ public class MilestoneMilestoneManager {
 		long idMilCh = mMilestone.getMilestoneChild().getIdMilestone();
 		Milestone master = em.find(Milestone.class, idMil);
 		Milestone child = em.find(Milestone.class, idMilCh);
-		System.out.println(master + " - " + child);
-		System.out.println(master.getMilestoneMilestone().size() + " - " + child.getMilestoneMilestone().size());
-		// check via getHierarchy //
 		boolean ok = true;
-		List<Milestone> mListA = milestoneManager.getHierarchy(master);
 		List<Milestone> mListB = milestoneManager.getHierarchy(child);
-		System.out.println(mListA.size());
-		System.out.println(mListB.size());
-		for (int i = 0; i < mListA.size(); i++) {
-			System.out.println(mListA.get(i).getIdMilestone() + " - " + child.getIdMilestone());
-			if (mListA.get(i).getIdMilestone().equals(child.getIdMilestone())) {
-				ok = false;
-				break;
-			}
+		// check per la presenza di una MilestoneMilestone uguale nel db
+		List<MilestoneMilestone> milestoneMilestoneList = em
+				.createQuery("FROM MilestoneMilestone WHERE milestone= :milestoneA AND milestoneChild= :milestoneB",
+						MilestoneMilestone.class)
+				.setParameter("milestoneA", master).setParameter("milestoneB", child).getResultList();
+		if (!milestoneMilestoneList.isEmpty()) {
+			ok = false;
 		}
+		// check via getHierarchy se nella gerarchia della child e' presente la
+		// master//
 		for (int i = 0; i < mListB.size(); i++) {
 			System.out.println(mListB.get(i).getIdMilestone() + " - " + master.getIdMilestone());
 			if (mListB.get(i).getIdMilestone().equals(master.getIdMilestone())) {
@@ -66,40 +63,6 @@ public class MilestoneMilestoneManager {
 				break;
 			}
 		}
-
-		/*
-		 * if (milestoneA.getIdMilestone().equals(milestoneB.getIdMilestone()))
-		 * { // OK // equals sulla milestone restituisce: //
-		 * it.finsoft.entity.Milestone@5f7b9c21 equals //
-		 * it.finsoft.entity.Milestone@3e730583 // funziona sull'ID // ERRORE
-		 * LOOP System.out.println("nodo=foglia"); ok = false; } else {
-		 * List<MilestoneMilestone> milestoneMilestoneList = em
-		 * .createQuery("FROM MilestoneMilestone WHERE milestone= :milestoneA AND milestoneChild= :milestoneB"
-		 * , MilestoneMilestone.class) .setParameter("milestoneA",
-		 * milestoneA).setParameter("milestoneB", milestoneB).getResultList();
-		 * if (!milestoneMilestoneList.isEmpty()) { // OK // E' gia presente una
-		 * MilestoneMilestone fatta cosi' System.out.println("gia' presente");
-		 * ok = false; }
-		 * 
-		 * // FATTO COSI NON FUNZIONA CORRETTAMENTE
-		 * 
-		 * List<Milestone> mListA = milestoneManager.getHierarchy(milestoneA);
-		 * List<Milestone> mListB = milestoneManager.getHierarchy(milestoneB);
-		 * 
-		 * for (int i = 0; i < mListA.size(); i++) {
-		 * System.out.println(mListA.get(i).getIdMilestone() + " - " +
-		 * milestoneB.getIdMilestone()); if
-		 * (mListA.get(i).getIdMilestone().equals(milestoneB.getIdMilestone()))
-		 * {// OK // // // ERRORE // LOOP
-		 * 
-		 * ok = false; break; } } for (int i = 0; i < mListB.size(); i++) {
-		 * System.out.println(mListB.get(i).getIdMilestone() + " - " +
-		 * milestoneA.getIdMilestone()); if
-		 * (mListB.get(i).getIdMilestone().equals(milestoneA.getIdMilestone()))
-		 * { ok = false; } }
-		 * 
-		 * }
-		 */
 		if (ok == true) {
 			return save(mMilestone);
 		} else {
