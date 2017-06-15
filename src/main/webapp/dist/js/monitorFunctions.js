f79931bb2bfb7df30f44fc6b00bad8ec55e343ce
Federico Cosco <federicocosco@outlook.com>
2017-06-15 17:53:23 +0200

    fixx

--- src/main/webapp/dist/js/monitorFunctions.js
+++ src/main/webapp/dist/js/monitorFunctions.js
@@ -96,12 +96,40 @@ function getDettaglioCalendarioMilestone(idCalendario) {
 	     }
 	   });
 }
-
+function pad(numb) {
+    return (numb < 10 ? '0' : '') + numb;
+}
 function polling(datatable, numRiga, data) {
-    var milestone = data.milestone.descrizione;
-    var tags = data.tags;
-    var endpoint = "ws/Polling?milestone=" + milestone
-      + "&tag=" + tags.split(",").join("&tag=");
+    
+	var milestone = data.milestone.descrizione;
+    var tags = data.tags;    
+    var endpoint = "ws/Polling?milestone=" + milestone + "&tag=" + tags.split(",").join("&tag=");
+
+    var _ora = data.ora;
+    
+    var _now = new Date($.now());
+    var nYear  = pad(_now.getFullYear());
+    var nMonth = pad(_now.getMonth() + 1);
+    var nDay   = pad(_now.getDate());
+    var nHours = pad(_now.getHours()); //returns 0-23
+    var nMinutes = pad(_now.getMinutes()); //returns 0-59
+    var nSeconds = pad(_now.getSeconds()); //returns 0-59    
+      
+    var _data = new Date(data.dataOraPreviste);
+    var dYear  = pad(_data.getFullYear());
+    var dMonth = pad(_data.getMonth() + 1);
+    var dDay   = pad(_data.getDate());
+    var dHours = pad(_data.getHours()); //returns 0-23
+    var dMinutes = pad(_data.getMinutes()); //returns 0-59
+    var dSeconds = pad(_data.getSeconds()); //returns 0-59    
+   
+
+    _now = parseInt(nYear+nMonth+nDay+nHours+nMinutes+nSeconds);
+    _data = parseInt(dYear+dMonth+dDay+dHours+dMinutes+dSeconds);
+    
+
+    var diff = _now - _data;    
+    
      $.ajax({
        type : "GET",
        url : endpoint,
@@ -109,10 +137,14 @@ function polling(datatable, numRiga, data) {
        success : function(dataSet) {
     	   
            if (dataSet != null) {
-               if (dataSet == 0) {
-            	   dataSet = '<span class="btn btn-danger btn-circle btn-sm btn-semaforo" style="width:15px; height: 15px; font-size:0%">0 rosso</span>';
+               if (dataSet == 0 && diff < 0) {
+            	   dataSet = '<span class="btn btn-primary btn-circle btn-sm btn-semaforo" style="width:15px; height: 15px; font-size:0%; cursor: default;">3 blu</span>';
+               } else if (dataSet == 0) {
+            	   dataSet = '<span class="btn btn-danger btn-circle btn-sm btn-semaforo" style="width:15px; height: 15px; font-size:0%; cursor: default;">0 rosso</span>';
                } else if (dataSet == 1) {
-            	   dataSet = '<span class="btn btn-success btn-circle btn-sm btn-semaforo" style="width:15px; height: 15px; font-size:0%">1 verde</span>';
+            	   dataSet = '<span class="btn btn-success btn-circle btn-sm btn-semaforo" style="width:15px; height: 15px; font-size:0%; cursor: default;">1 verde</span>';
+               } else {
+            	   dataSet = '';
                }
            }
     	   datatable.cell(numRiga, 3).data(dataSet).draw();
