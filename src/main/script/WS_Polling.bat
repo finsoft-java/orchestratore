370d9010f4c99a880dbf51c9555027fb2ef0e4d9
Finsoft-PC <Finsoft-PC@192.168.1.221>
2017-06-16 09:07:20 +0200

    Update 16062017 - Script .bat
    Aggiornati gli script del Polling in modo che accettino un solo TAG

--- src/main/script/WS_Polling.bat
+++ src/main/script/WS_Polling.bat
@@ -3,9 +3,8 @@ set WGET=c:\Users\Finsoft-PC\Desktop\Progetto\VisualWGet\wget.exe
 set ENDPOINT=http://localhost:8080/orchestratoreRADAR/ws/Polling
 set MILESTONE=ACQUISIZIONE DATI MACS1
 
-rem Questo .bat richiede l'indicazione del semaforo che si vuole creare e del quale conosciamo il numero esatto di TAG
-rem da ricercare
+rem Questo .bat richiede l'indicazione della Milestone che si vuole creare e del quale conosciamo il TAG da ricercare
 
-%WGET% -qO- "%ENDPOINT%?milestone=%MILESTONE%&tag=%1&tag=%2&tag=%3&tag=%4&tag=%5&tag=%6&tag=%7&tag=%8&tag=%9"
+%WGET% -qO- "%ENDPOINT%?milestone=%MILESTONE%&tag=%1"
 PAUSE
 
