d97dc790e354d01c052f86f2bf3f6f635eea9d9a
Luca vercelli <luca.vercelli.to@gmail.com>
2017-07-13 10:40:59 +0200

    render timestamp

--- src/main/webapp/dist/js/eventi.js
+++ src/main/webapp/dist/js/eventi.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	caricaEventi();
 })
 
+function renderTimestamp(data, type, row, meta) {
+	if (data == null) return null;
+	return data.substring(8,10) + '/'+data.substring(5,7)+'/'+data.substring(0,4)+ ' ' + data.substring(11,19);
+}
 
 function caricaEventi() {
 	
@@ -22,7 +26,7 @@ function caricaEventi() {
 			dataSrc: ''
 		},
 		columns : [
-			{data : 'tStampEvento', className : 'col-md-2', defaultContent : '' },
+			{data : 'tStampEvento', className : 'col-md-2', defaultContent : '', render: renderTimestamp },
 			{data : 'tipoEvento.codice', className : 'col-md-2', defaultContent : '' },
 			{data : 'entita.codice', className : 'col-md-2', defaultContent : ''}, 
 			{data : 'entita.acronimo', className : 'col-md-2', defaultContent : ''},
