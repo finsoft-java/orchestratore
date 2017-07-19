9c22994271f1aece62db0a0980cb51256680827d
Luca vercelli <luca.vercelli.to@gmail.com>
2017-07-19 15:34:49 +0200

    preparato nuovo WS partcol

---
+++ src/main/java/it/finsoft/resources/WSPartCol.java
@@ -0,0 +1,24 @@
+package it.finsoft.resources;
+
+import javax.ejb.Stateless;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("PartCol")
+@Produces({ MediaType.TEXT_PLAIN })
+public class WSPartCol {
+
+	@GET
+	public String get(@QueryParam("anno") String anno, @QueryParam("mese") String mese, @QueryParam("giro") String giro,
+			@QueryParam("lettera") String lettera) {
+
+		// TODO
+		// es. P2017051LS
+		// Ma devo accedere alla funzione GET_PART_COL di U7B20...
+		return null;
+	}
+}
