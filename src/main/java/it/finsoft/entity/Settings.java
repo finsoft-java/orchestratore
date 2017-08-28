0123f88660d35d94ba07fad398918560020005bc
Luca vercelli <luca.vercelli.to@gmail.com>
2017-08-28 16:58:55 +0200

    settings

---
+++ src/main/java/it/finsoft/entity/Settings.java
@@ -0,0 +1,48 @@
+package it.finsoft.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * You can only have 1 row in this table.
+ * 
+ * @author u0i8226
+ * 
+ */
+@Entity
+@Table(name = "U7B2_OWN.T_BDOR0_SETTINGS")
+@XmlRootElement
+public class Settings implements Serializable {
+
+	private static final long serialVersionUID = 3945515390166304783L;
+
+	private Long id = 1L;
+	private String wsPath = "http://localhost:7001/bdor0ws";
+
+	@Id
+	@Column(name = "ID", unique = true)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		if (id != 1L)
+			throw new IllegalArgumentException("Cannot create another instance of this class");
+		this.id = id;
+	}
+
+	@Column(name = "WS_PATH")
+	public String getWsPath() {
+		return wsPath;
+	}
+
+	public void setWsPath(String wsPath) {
+		this.wsPath = wsPath;
+	}
+
+}
